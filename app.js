e97fe632e933049b22d08c814ef9ee1d05a47f1f
clement <creations@connorkealey.design>
2023-03-01 21:34:08 -0800

    Templatized pages and header

--- app.js
+++ app.js
@@ -1,34 +1,67 @@
 var express = require('express');
+
+//Create express app
 var app = express();
+PORT = 15646;
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
-PORT = 15646;
+
+//serves stactic files like CSS from the public directory
+app.use(express.static('public'));
+
+//loads handlebars module
+var handlebars = require('express-handlebars');
+
+
+app.engine('hbs', handlebars.engine({
+    defaultLayout: 'main',
+    layoutsDir: __dirname + '/views/layouts',
+    partialsDir: __dirname + '/views/partials/',
+    extname: 'hbs'
+}));
+app.set('view engine', 'hbs');
 
 //set up database file from connector
 var db = require('./db-connector.js');
 
-
-var exphbs = require('express-handlebars');
-const { query } = require('express');
-app.engine('.hbs', exphbs.engine({
-    extname: ".hbs"
-}));
-app.set('view engine', '.hbs');
-
-app.use(express.static('public'));
-
-//render index
+//routing
 app.get('/', function (req, res) {
-    res.render('index');
+    res.render('index', {title: 'Home'});
+});
+
+app.get('/artists', function (req, res) {
+    res.render('artists', {title: 'Artists'});
+});
+
+app.get('/frames', function (req, res) {
+    res.render('frames', {title: 'Frames'});
+});
+
+app.get('/artwork', function (req, res) {
+    res.render('artwork', {title: 'Artwork'});
 });
 
 app.get('/patrons', function (req, res) {
-    let query1 = "SELECT patron_id, first_name, last_name, email, address, is_artist FROM Patrons;";
-    db.pool.query(query1, function (error, rows, fields) {
-        res.render('patrons', { data: rows });
-    })
+    res.render('patrons', {title: 'Patrons'});
 });
+
+app.get('/transactions', function (req, res) {
+    res.render('transactions', {title: 'Transactions'});
+});
+
+const { query } = require('express');
+
+
+//Things Connor hasn't wrapped his head around
+
+// app.get('/patrons', function (req, res) {
+//     let query1 = "SELECT patron_id, first_name, last_name, email, address, is_artist FROM Patrons;";
+//     db.pool.query(query1, function (error, rows, fields) {
+//         res.render('patrons', { data: rows });
+//     })
+// });
 
 app.post('/add-patron-set-form', function (req, res) {
     let data = req.body;
